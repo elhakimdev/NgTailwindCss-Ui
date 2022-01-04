daeade5024172471e389ffa4ee69476a7e1948cd
ElhkaimDev <abdulelhakim68@gmial.com>
2022-01-05 04:39:13 +0700

    set attribute aria expanded after click

--- src/app/component/accordion/accordion-list/accordion-list.component.ts
+++ src/app/component/accordion/accordion-list/accordion-list.component.ts
@@ -1,9 +1,11 @@
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component, Input, OnInit } from '@angular/core';
+import { elementAt } from 'rxjs';
 
 @Component({
   selector: 'app-accordion-list',
   templateUrl: './accordion-list.component.html',
-  styleUrls: ['./accordion-list.component.css']
+  styleUrls: ['./accordion-list.component.css'],
 })
 export class AccordionListComponent implements OnInit {
   @Input() HeaderText: string = '';
@@ -15,8 +17,12 @@ export class AccordionListComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  toogleShowContent(){
+  toogleShowContent(event: Event){
     this.isExpanded = !this.isExpanded;
+    
+    let currentElement = (event.target as Element)
+
+    currentElement.parentElement?.setAttribute('aria-expanded', 'true');
   }
 
 }
--- src/app/component/accordion/accordion.component.ts
+++ src/app/component/accordion/accordion.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-accordion',
@@ -7,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AccordionComponent implements OnInit {
 
+  @Input() collapse: string = '';
+
   constructor() { }
 
   ngOnInit(): void {
+    if (this.collapse === 'auto') {
+      console.log(document.getElementsByTagName('app-accordion-list'));
+    }
   }
 
 }
