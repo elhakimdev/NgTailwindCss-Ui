58c1869d883d86f606abc8a9e284adc5b15e6297
ElhkaimDev <abdulelhakim68@gmial.com>
2022-01-05 00:02:07 +0700

    base accordion layout

---
+++ src/app/component/component.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AccordionModule } from './accordion/accordion.module';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    AccordionModule
+  ]
+})
+export class ComponentModule { }
