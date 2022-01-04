c7877e186333eb46cb2e01b2321c4b286a43d25a
ElhkaimDev <abdulelhakim68@gmial.com>
2022-01-05 05:15:49 +0700

    gagal total

--- src/app/component/accordion/accordion.component.ts
+++ src/app/component/accordion/accordion.component.ts
@@ -1,20 +1,33 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, ComponentFactoryResolver, Input, OnChanges, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-accordion',
   templateUrl: './accordion.component.html',
   styleUrls: ['./accordion.component.css']
 })
-export class AccordionComponent implements OnInit {
+export class AccordionComponent implements OnInit, OnChanges {
 
   @Input() collapse: string = '';
 
   constructor() { }
 
   ngOnInit(): void {
-    if (this.collapse === 'auto') {
-      console.log(document.getElementsByTagName('app-accordion-list'));
-    }
+    // if (this.collapse === 'auto') {
+    //   const listElements = document.querySelectorAll('#acccordion-list');
+    //   for (let i = 0; i < listElements.length; i++) {
+    //     const element = listElements[i].hasAttribute('aria-expanded');
+    //     console.log(element);        
+    //   }
+    // }
+  }
+  ngOnChanges():void{
+    // if (this.collapse === 'auto') {
+    //   const listElements = document.querySelectorAll('#acccordion-list');
+    //   for (let i = 0; i < listElements.length; i++) {
+    //     const element = listElements[i].hasAttribute('aria-expanded');
+    //     console.log(element);        
+    //   }
+    // }
   }
 
 }
